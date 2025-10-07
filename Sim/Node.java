25e5851595d74aa395796d31dbbac6a3b5a0ef75
Leo Man <leoman3628@gmail.com>
2025-10-07 17:06:43 +0200

    Gaussian and poisson node deleted, instead implemented by Node. Simulation writes result to csv files.

--- Sim/Node.java
+++ Sim/Node.java
@@ -3,81 +3,172 @@ package Sim;
 // This class implements a node (host) it has an address, a peer that it communicates with
 // and it count messages send and received.
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.Objects;
 import java.util.Random;
 
 public class Node extends SimEnt {
-	private NetworkAddr _id;
-	private SimEnt _peer;
-	private int _sentmsg=0;
-	private int _seq = 0;
+    private NetworkAddr _id;
+    private SimEnt _peer;
+    private int _sentmsg=0;
+    private int _seq = 0;
 
-	Random r = new Random(3);
-	
-	public Node (int network, int node)
-	{
-		super();
-		_id = new NetworkAddr(network, node);
-	}	
-	
-	
-	// Sets the peer to communicate with. This node is single homed
-	
-	public void setPeer (SimEnt peer)
-	{
-		_peer = peer;
-		
-		if(_peer instanceof Link )
-		{
-			 ((Link) _peer).setConnector(this);
-		}
-	}
-	
-	
-	public NetworkAddr getAddr()
-	{
-		return _id;
-	}
-	
-//**********************************************************************************	
-	// Just implemented to generate some traffic for demo.
-	// In one of the labs you will create some traffic generators
-	
-	private int _stopSendingAfter = 0; //messages
-	private int _timeBetweenSending = 0; //time between messages
-	private int _toNetwork = 0;
-	private int _toHost = 0;
-	
-	public void StartSending(int network, int node, int number, int timeInterval, int startSeq)
-	{
-		_stopSendingAfter = number;
-		_timeBetweenSending = timeInterval;
-		_toNetwork = network;
-		_toHost = node;
-		_seq = startSeq;
-		send(this, new TimerEvent(),0);	
-	}
-	
-//**********************************************************************************	
-	
-	// This method is called upon that an event destined for this node triggers.
-	
-	public void recv(SimEnt src, Event ev)
-	{
-		if (ev instanceof TimerEvent)
-		{			
-			if (_stopSendingAfter > _sentmsg)
-			{
-				_sentmsg++;
-				send(_peer, new Message(_id, new NetworkAddr(_toNetwork, _toHost),_seq),0);
-				send(this, new TimerEvent(),_timeBetweenSending);
-				System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" sent message with seq: "+_seq + " at time "+SimEngine.getTime());
-				_seq++;
-			}
-		}
-		if (ev instanceof Message)
-		{
-			System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" receives message with seq: "+((Message) ev).seq() + " at time "+SimEngine.getTime());
-			
-		}
-	}
+
+
+    Random r = new Random(3);
+
+    public Node (int network, int node)
+    {
+        super();
+        _id = new NetworkAddr(network, node);
+    }
+
+
+    // Sets the peer to communicate with. This node is single homed
+
+    public void setPeer (SimEnt peer)
+    {
+        _peer = peer;
+
+        if(_peer instanceof Link )
+        {
+            ((Link) _peer).setConnector(this);
+        }
+    }
+
+
+    public NetworkAddr getAddr()
+    {
+        return _id;
+    }
+
+//**********************************************************************************
+    // Just implemented to generate some traffic for demo.
+    // In one of the labs you will create some traffic generators
+
+    private int _stopSendingAfter = 0; //messages
+    private double _timeBetweenSending = 0; //time between messages
+    private int _toNetwork = 0;
+    private int _toHost = 0;
+
+    private double median, stddev;
+
+    private String currentMode = "uniform";
+
+    public void StartSending(int network, int node, int number, int timeInterval, int startSeq)
+    {
+        this.currentMode = "uniform";
+        _stopSendingAfter = number;
+        _timeBetweenSending = timeInterval;
+        _toNetwork = network;
+        _toHost = node;
+        _seq = startSeq;
+        send(this, new TimerEvent(),0);
+    }
+
+    private double nextGauss(){
+        double time = r.nextGaussian(this.median, this.stddev);
+        if (time < 0){
+            time = r.nextGaussian(this.median, this.stddev);
+            this.nextGauss();
+        }
+        return time;
+    }
+    public void StartSendingGauss(int network, int node, int number, double timeInterval, double stddev, int startSeq)
+    {
+        this.median = timeInterval;
+        this.stddev = stddev;
+        this.currentMode = "gauss";
+
+        _stopSendingAfter = number;
+        _timeBetweenSending = nextGauss();
+        _toNetwork = network;
+        _toHost = node;
+        _seq = startSeq;
+        send(this, new TimerEvent(),0);
+    }
+
+
+    private static int getPoissonRandom(double mean) {
+        Random r = new Random();
+        double L = Math.exp(-mean);
+        int k = 0;
+        double p = 1.0;
+        do {
+            p = p * r.nextDouble();
+            k++;
+        } while (p > L);
+        return k - 1;
+    }
+    public void poissonStartSending(int network, int node, int number, double mean, int startSeq)
+    {
+        this.currentMode = "poisson";
+        this.median = mean;
+        _stopSendingAfter = number;
+        _timeBetweenSending = getPoissonRandom(mean);
+        _toNetwork = network;
+        _toHost = node;
+        _seq = startSeq;
+        send(this, new TimerEvent(),0);
+    }
+
+    private void updateTimeBetweenSending(){
+        if (Objects.equals(this.currentMode, "uniform")){
+            return;
+        }
+        if (Objects.equals(this.currentMode, "gauss")){
+            this._timeBetweenSending = nextGauss();
+        }
+        if (Objects.equals(this.currentMode, "poisson")){
+            this._timeBetweenSending = getPoissonRandom(this.median);
+        }
+
+    }
+
+//**********************************************************************************
+
+    // This method is called upon that an event destined for this node triggers.
+
+    public void recv(SimEnt src, Event ev)
+    {
+        if (ev instanceof TimerEvent)
+        {
+            if (_stopSendingAfter > _sentmsg)
+            {
+                _sentmsg++;
+                send(_peer, new Message(_id, new NetworkAddr(_toNetwork, _toHost),_seq),0);
+                send(this, new TimerEvent(),_timeBetweenSending);
+                updateTimeBetweenSending();
+
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(currentMode + ".csv", true));
+                    writer.write((_id.networkId() + "." + _id.nodeId() + "," + _seq + ',' + SimEngine.getTime()));
+                    writer.newLine();
+                    writer.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" sent message with seq: "+_seq + " at time "+SimEngine.getTime());
+                _seq++;
+            }
+        }
+        if (ev instanceof Message)
+        {
+
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter("out.csv", true));
+                writer.write(_id.networkId() + "." + _id.nodeId() + "," + ((Message) ev).seq() + ',' + SimEngine.getTime());
+                writer.newLine();
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" receives message with seq: "+((Message) ev).seq() + " at time "+SimEngine.getTime());
+
+        }
+    }
 }
--- Sim/Run.java
+++ Sim/Run.java
@@ -2,42 +2,54 @@ package Sim;
 
 // An example of how to build a topology and starting the simulation engine
 
+
 public class Run {
 	public static void main (String [] args)
 	{
+		new java.io.File("gauss.csv").delete();
+		new java.io.File("out.csv").delete();
+		new java.io.File("poisson.csv").delete();
+		new java.io.File("uniform.csv").delete();
 
 		Link link1 = new Link();
  		Link link2 = new Link();
 		Link link3 = new Link();
+		Link link4 = new Link();
 
 
-		
 
-		NodeGaussian host1 = new NodeGaussian(1, 1);
-		NodePoisson host2 = new NodePoisson(2,1);
-		Node host3 = new Node(3,1);
+
+
+		Node host1 = new Node(1, 1);
+		Node host2 = new Node(2, 1);
+		Node host3 = new Node(3, 1);
+		Node host4 = new Node(4, 1);
+		//NodePoisson host2 = new NodePoisson(2,1);
+		//Node host3 = new Node(3,1);
 
 
 		//Connect links to hosts
 		host1.setPeer(link1);
 		host2.setPeer(link2);
 		host3.setPeer(link3);
+		host4.setPeer(link4);
 
 		// Creates as router and connect
 		// links to it. Information about 
 		// the host connected to the other
 		// side of the link is also provided
 		// Note. A switch is created in same way using the Switch class
-		Router routeNode = new Router(3);
+		Router routeNode = new Router(4);
 		routeNode.connectInterface(0, link1, host1);
 		routeNode.connectInterface(1, link2, host2);
 		routeNode.connectInterface(2, link3, host3);
+		routeNode.connectInterface(3, link4, host4);
 		
 		// Generate some traffic
 
-		//host1.StartSending(3, 1, 5, 3, 10, 0);
-		host2.poissonStartSending(3,1, 5, 10, 0);
-		//host2.StartSending(3, 1, 5, 5, 10);
+		host1.StartSendingGauss(4, 1, 100, 5, 10, 1000);
+		host2.poissonStartSending(4,1, 100, 5, 2000);
+		host3.StartSending(4, 1, 100, 5, 3000);
 
 		// host2 will send 2 messages with time interval 10 to network 1, node 1. Sequence starts with number 10
 		//host2.StartSending(1, 1, 2, 10, 10);
