466e37a577207a6db834a24547cd848cb25cb259
Walle692 <146271763+Walle692@users.noreply.github.com>
2025-10-20 12:15:20 +0200

    receive Bu kanske funkar, oklart

--- Sim/BindingTableEntry.java
+++ Sim/BindingTableEntry.java
@@ -2,50 +2,44 @@ package Sim;
 
 public class BindingTableEntry {
 
-    private SimEnt _node;
-    private int _hoa;
-    private int _coa;
+    private NetworkAddr _hoa;
+    private NetworkAddr _coa;
     private int _lifetime;
 
-    BindingTableEntry(SimEnt node, int hoa, int coa, int lifetime)
+    BindingTableEntry(NetworkAddr hoa, NetworkAddr coa, int lifetime)
     {
-        _node = node;
         _hoa = hoa;
         _coa = coa;
         _lifetime = lifetime;
     }
 
-    protected int get_coa()
+    public NetworkAddr get_coa()
     {
         return _coa;
     }
 
-    protected int get_hoa()
+    public NetworkAddr get_hoa()
     {
         return _hoa;
     }
 
-    protected int get_lifetime()
+    public int get_lifetime()
     {
         return _lifetime;
     }
 
-    protected SimEnt node()
-    {
-        return _node;
-    }
 
-    protected void set_lifetime(int lifetime)
+    public void set_lifetime(int lifetime)
     {
         _lifetime = lifetime;
     }
 
-    protected  void set_coa(int coa)
+    public  void set_coa(int coa)
     {
         _coa = coa;
     }
 
-    protected   void set_hoa(int hoa)
+    public   void set_hoa(int hoa)
     {
         _hoa = hoa;
     }
